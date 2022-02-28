a6f3c4f3a78dae2006acca0018dde789c5633f5f
Amkam <amaduskamara@gmail.com>
2022-02-28 11:28:16 +0000

    Add instance variables9setters and getters)

--- animal.rb
+++ animal.rb
@@ -6,6 +6,26 @@ class Animal
     @type = type
   end
 
+  def id
+    @id
+  end
+
+  def type
+    @type
+  end
+
+  def number_of_legs
+    @number_of_legs
+  end
+
+  def name
+    @name
+  end
+
+  def name=(value)
+    @name = value
+  end
+
   def speak()
     puts "Muaahhh, animal speaks :)"
   end
